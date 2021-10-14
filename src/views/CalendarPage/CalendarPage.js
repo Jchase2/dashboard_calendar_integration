f8b0cdee812edcf2e02f2fc9f8f737849cf30abc
jchase2 <mr.jdchase@gmail.com>
2021-10-13 23:31:20 -0400

    Updated react-swipeable-views from 13.9 to 14 to fix compatibility issue with react, installed react-big-calendar with moment as its localizer. Created CalendarPage directory in views, created CalendarPage.js and event.js (hardcoded calendar events).

---
+++ src/views/CalendarPage/CalendarPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import moment from "moment";
+import events from "./events";
+
+const localizer = momentLocalizer(moment);
+
+export default function CalendarPage() {
+  return (
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
+    </div>
+  );
+}
---
+++ src/views/CalendarPage/events.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    id: 0,
+    title: "Here's an event with a title",
+    allDay: true,
+    start: new Date(2021, 9, 1),
+    end: new Date(2021, 9, 1),
+  },
+  {
+    id: 1,
+    title: "Long Event",
+    start: new Date(2021, 9, 3),
+    end: new Date(2021, 9, 5),
+  },
+
+  {
+    id: 2,
+    title: "KANYE",
+    start: new Date(2021, 9, 20),
+    end: new Date(2021, 9, 20),
+  },
+  {
+    id: 3,
+    title: "Some Event",
+    start: new Date(2021, 9, 9),
+    end: new Date(2021, 9, 10),
+  },
+  {
+    id: 4,
+    title: "Conference",
+    start: new Date(2021, 9, 11),
+    end: new Date(2021, 9, 13),
+    desc: "Big conference for important people",
+  },
+];
